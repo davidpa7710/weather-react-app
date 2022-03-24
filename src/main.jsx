e3fee56b17f4d187da7e0d06f8334308f3a3b14c
David Palacios <davidpa7710@gmail.com>
2022-03-23 21:30:22 -0600

    create interface with html and css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import './styles/index.css'
+import { Weather } from './components/weather/index'
+
+// console.log(import.meta.env.APP_API_KEY);
 
 render(
   <StrictMode>
-    <p>App</p>
+    <Weather />
   </StrictMode>,
   document.getElementById('root')
 )
